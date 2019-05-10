a6e469bfca6c56a9d894547a18bf9c3c2d523a43
Mark Golazeski <mgolazeski@convoy.com>
2019-05-09 17:39:11 -0700

    Add concrete implmentation that checks for schema changes

--- core/dbt/adapters/sql/impl.py
+++ core/dbt/adapters/sql/impl.py
@@ -104,6 +104,59 @@ class SQLAdapter(BaseAdapter):
 
                 self.alter_column_type(current, column_name, new_type)
 
+    def has_schema_changed(self, goal, current):
+        """
+        Look for schema changes between the target columns and reference columns
+        Step through each column and return that a schema change *has* happened
+        if any of the following are true:
+        1. Number of columns are different (column has been added or removed)
+        2. Columns have different names
+        3. Columns have different data type
+        4. Columns have different data type size
+        """
+        reference_columns = {
+            c.name: c for c in
+            self.get_columns_in_relation(goal)
+        }
+
+        # Theoretically this works, but postgres temporary tables are held within their
+        # own schema, so it's not possible to track the differences.
+        # This logic should work with Snowflake based on manual testing, but not confirmed
+        # in this code path.
+        target_columns = {
+            c.name: c for c in
+            self.get_columns_in_relation(current)
+        }
+
+        # 1. The schema has changed if columns have been added or removed
+        if len(reference_columns) != len(target_columns):
+            logger.debug("Schema difference detected: Reason 1")
+            return True
+
+        for reference_column_name, reference_column in reference_columns.items():
+            target_column = target_columns.get(reference_column_name)
+            # 2a. The schema has changed if a reference column is not found in the target columns
+            if target_column is None:
+                logger.debug("Schema difference detected: Reason 2a")
+                return True
+
+            # 3/4. If the columns do not have the same data type and size (see core/dbt/schema.py for more details)
+            if reference_column.data_type != target_column.data_type:
+                logger.debug("Schema difference detected: Reason 3/4")
+                return True
+
+        for i, target_column_name in enumerate(target_columns):
+            reference_column = reference_columns.get(target_column_name)
+            target_column = target_columns.get(target_column_name)
+            # 2b. The schema has changed if a target column is not found in the reference columns
+            if reference_column is None:
+                logger.debug("Schema difference detected: Reason 2b")
+                return True
+
+        # Nothing has detected as changed
+        logger.debug("No schema difference detected")
+        return False
+
     def alter_column_type(self, relation, column_name, new_column_type):
         """
         1. Create a new column (w/ temp name and correct type)

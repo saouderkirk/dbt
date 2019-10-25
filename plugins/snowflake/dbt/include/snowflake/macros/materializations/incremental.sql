b307ac621c62607a725064b85b409e4724bb30b1
Chris Fraleigh <chrisfraleigh@c02xr9m8jg5h.internal.convoy.com>
2019-10-25 08:11:08 -0700

    moving block to better spot

--- plugins/snowflake/dbt/include/snowflake/macros/materializations/incremental.sql
+++ plugins/snowflake/dbt/include/snowflake/macros/materializations/incremental.sql
@@ -3,12 +3,7 @@
 
   {%- set unique_key = config.get('unique_key') -%}
   {%- set full_refresh_mode = (flags.FULL_REFRESH == True) -%}
-  {%- if config.get('on_schema_change') == 'full_refresh' and adapter.target_contains_schema_change(old_relation=old_relation, to_relation=target_relation) -%}
-    {%- set full_refresh_mode = True -%}
-  {%- endif -%}
-  {% if config.get('on_schema_change'== 'fail') and adapter.target_contains_schema_change(old_relation=old_relation, to_relation=target_relation) -%}
-    {{ exceptions.raise_fail_on_schema_change() }}
-  {%- endif %}
+  
   {%- set identifier = model['alias'] -%}
 
   {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
@@ -29,6 +24,13 @@
   {% if strategy not in ['merge', 'delete+insert'] %}
     {% do exceptions.raise_compiler_error(invalid_strategy_msg) %}
   {% endif %}
+
+  {%- if config.get('on_schema_change') == 'full_refresh' and adapter.target_contains_schema_change(old_relation=old_relation, to_relation=target_relation) -%}
+    {%- set full_refresh_mode = True -%}
+  {%- endif -%}
+  {% if config.get('on_schema_change'== 'fail') and adapter.target_contains_schema_change(old_relation=old_relation, to_relation=target_relation) -%}
+    {{ exceptions.raise_fail_on_schema_change() }}
+  {%- endif %}
 
   -- setup
   {{ run_hooks(pre_hooks, inside_transaction=False) }}

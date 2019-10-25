45b4fbcf7a2019953e1d040b0b76ca32011da827
Chris Fraleigh <chrisfraleigh@c02xr9m8jg5h.internal.convoy.com>
2019-10-25 14:02:41 -0700

    making sure the relation build is the same

--- plugins/snowflake/dbt/include/snowflake/macros/materializations/incremental.sql
+++ plugins/snowflake/dbt/include/snowflake/macros/materializations/incremental.sql
@@ -5,10 +5,9 @@
   {%- set full_refresh_mode = (flags.FULL_REFRESH == True) -%}
   {%- set on_schema_change =  config.get('on_schema_change') -%}
   {%- set identifier = model['alias'] -%}
-
   {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
-  {%- set target_relation = api.Relation.create(database=database,schema=schema,identifier=identifier, type='table') -%}
-  {%- set tmp_relation = make_temp_relation(target_relation) -%}
+  {%- set target_relation = api.Relation.create(identifier=identifier, schema=schema, database=database,  type='table') -%}
+  {%- set tmp_relation = make_temp_relation(target_relation) %}
 
   {#-- Find and validate the incremental strategy #}
   {%- set strategy = config.get("incremental_strategy", default="merge") -%}
